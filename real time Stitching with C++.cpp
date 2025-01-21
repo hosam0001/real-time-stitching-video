46be8db818ca9377b3c146609b234b2b67806aca
hosam0001 <hosampedri.2000@gmail.com>
2025-01-21 13:40:14 +0200

    Hosam Update C++ code
    // to install YOLO v5
    python export.py --weights yolov5s.pt --include onnx

--- real time Stitching with C++.cpp
+++ real time Stitching with C++.cpp
@@ -1,94 +1,124 @@
-#include "MareArtsStitcher.h"
 #include "opencv2/opencv.hpp"
 #include <vector>
+#include <thread>
 using namespace cv;
 using namespace std;
 
+void detectObjects(Mat& frame) {
+    string modelPath = "yolov5s.onnx";
+    Net net = readNet(modelPath);
+    Mat blob;
+    blobFromImage(frame, blob, 1 / 255.0, Size(640, 640), Scalar(0, 0, 0), true, false);
+    net.setInput(blob);
+    vector<Mat> outputs;
+    net.forward(outputs, net.getUnconnectedOutLayersNames());
+    for (size_t i = 0; i < outputs.size(); ++i) {
+        float* data = (float*)outputs[i].data;
+        for (int j = 0; j < outputs[i].rows; ++j, data += outputs[i].cols) {
+            float confidence = data[4];
+            if (confidence > 0.5) {
+                int centerX = (int)(data[0] * frame.cols);
+                int centerY = (int)(data[1] * frame.rows);
+                int width = (int)(data[2] * frame.cols);
+                int height = (int)(data[3] * frame.rows);
+                int left = centerX - width / 2;
+                int top = centerY - height / 2;
+                rectangle(frame, Point(left, top), Point(left + width, top + height), Scalar(0, 255, 0), 2);
+            }
+        }
+    }
+}
+
+void processFrame(VideoCapture& cap, Mat& frame, bool& running) {
+    while (running) {
+        Mat temp;
+        cap >> temp;
+        if (!temp.empty()) {
+            detectObjects(temp);
+            frame = temp.clone();
+        }
+    }
+}
 
 int main(int argc, char** argv)
 {
-    unsigned long AAtime=0, BBtime=0; 
-
     VideoCapture cap1, cap2;
     namedWindow("cam1", 0);
     namedWindow("cam2", 0);
-    namedWindow("result", WINDOW_AUTOSIZE); //WINDOW_NORMAL);
-    if(!cap1.open(0))
-        return 0;
-    cap1.set(CAP_PROP_FRAME_WIDTH,640);
-    cap1.set(CAP_PROP_FRAME_HEIGHT,480);
+    namedWindow("result", WINDOW_AUTOSIZE);
 
-    if(!cap2.open(2))
-        return 0;
-    cap2.set(CAP_PROP_FRAME_WIDTH,640);
-    cap2.set(CAP_PROP_FRAME_HEIGHT,480);
+    if (!cap1.open(0)) {
+        cout << "Error: Failed to open camera 1." << endl;
+        return -1;
+    }
+    cap1.set(CAP_PROP_FRAME_WIDTH, 320);
+    cap1.set(CAP_PROP_FRAME_HEIGHT, 240);
 
-    MareArtsStitcher stitcher;
-    std::string MYSN = "your_sn_number";
-	stitcher.m_ImageWarppingWay = "plane"; //"cylindrical";
-    stitcher.m_FeatureFindAlgorithm = "akaze";
-    if (stitcher.check_SNcertification("my@email.com",MYSN) == false)
-	{
-		std::cout << "SN certification fail" << std::endl;
-		return 0;
-	}
+    if (!cap2.open(2)) {
+        cout << "Error: Failed to open camera 2." << endl;
+        return -1;
+    }
+    cap2.set(CAP_PROP_FRAME_WIDTH, 320);
+    cap2.set(CAP_PROP_FRAME_HEIGHT, 240);
 
+    Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA);
 
-    int mode=0;
+    Mat frame1, frame2;
+    bool running = true;
+
+    thread t1(processFrame, ref(cap1), ref(frame1), ref(running));
+    thread t2(processFrame, ref(cap2), ref(frame2), ref(running));
+
+    int mode = 0;
     while (true) {
+        if (!frame1.empty()) imshow("cam1", frame1);
+        if (!frame2.empty()) imshow("cam2", frame2);
 
-        Mat cameraFrame1;
-        cap1 >> cameraFrame1; //get one frame form video
-        Mat cameraFrame2;
-        cap2 >> cameraFrame2; //get one frame form video
-    
-        //if(mode == 0)
-        {
-            imshow("cam1", cameraFrame1);
-            imshow("cam2", cameraFrame2);
-        }
+        if (mode == 1) {
+            printf("Panorama parameter processing...\n");
+            vector<Mat> vImg_color;
+            vImg_color.push_back(frame1);
+            vImg_color.push_back(frame2);
 
-        if(mode == 1)
-        {
-            printf("panorama parameter processing...\n");
-            vector< Mat > vImg_color;
-            vImg_color.push_back( cameraFrame1 );
-            vImg_color.push_back( cameraFrame2 );
-            AAtime = getTickCount();
-            if( stitcher.calibrateFrame(vImg_color) == false)
-                cout << "stitching fail " << endl << endl;
-            else
+            Mat result;
+            Stitcher::Status status = stitcher->stitch(vImg_color, result);
+
+            if (status != Stitcher::OK) {
+                cout << "Stitching failed! Error code: " << int(status) << endl;
+            } else {
                 mode = 2;
-            //Processing time measurement   
-            BBtime = getTickCount(); 
-            printf("Processing time = %.2lf(sec) \n",  (BBtime - AAtime)/getTickFrequency() );      
+            }
         }
 
-        if(mode == 2){
-            vector< Mat > vImg_color;
-            vImg_color.push_back( cameraFrame1 );
-            vImg_color.push_back( cameraFrame2 );
-            
-            AAtime = getTickCount();
-            cv::Mat result;
-            if (stitcher.stitcher(vImg_color, result))
-                cv::imshow("result", result);
-            
-            //Processing time measurement   
-            BBtime = getTickCount(); 
-            printf("Processing time = %.2lf(sec) \n",  (BBtime - AAtime)/getTickFrequency() ); 
+        if (mode == 2) {
+            vector<Mat> vImg_color;
+            vImg_color.push_back(frame1);
+            vImg_color.push_back(frame2);
+
+            Mat result;
+            Stitcher::Status status = stitcher->stitch(vImg_color, result);
+
+            if (status == Stitcher::OK) {
+                imshow("result", result);
+            } else {
+                cout << "Stitching failed! Error code: " << int(status) << endl;
+            }
         }
 
         char k = waitKey(10);
-            if ( k == 'p') //processing command
-            mode=1;
-            else if (k == 'r') //reset camera mode
-            mode=0;
-            else if (k == 'q') //quit
+        if (k == 'p') {
+            mode = 1;
+        } else if (k == 'r') {
+            mode = 0;
+        } else if (k == 'q') {
+            running = false;
             break;
         }
+    }
+
+    t1.join();
+    t2.join();
 
     destroyAllWindows();
-    
     return 0;
 }
